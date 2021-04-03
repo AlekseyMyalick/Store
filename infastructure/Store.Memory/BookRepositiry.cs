8fc1d9e486f249b464cff950951550ca09d9ff27
Alexey Myalik <myalick2017@yandex.ru>
2021-04-03 19:36:37 +0300

    add a book description page.

--- infastructure/Store.Memory/BookRepositiry.cs
+++ infastructure/Store.Memory/BookRepositiry.cs
@@ -6,9 +6,12 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12312-31231", "D. Knuth", "art of Programming"),
-            new Book(2, "ISBN 12312-31232", "M. Fowier", "Refactoring"),
-            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Richie" ,"C Programming Language"),
+            new Book(1, "ISBN 0201038013", "D. Knuth", "Art of Programming, Vol. 1",
+                "This volume begins with basic programming and techniq", 7.19m),
+            new Book(2, "ISBN 02010485672", "M. Fowier", "Refactoring",
+                "As the applicationg of opject technology--particulary the Java", 12.45m),
+            new Book(3, "ISBN 0131101633", "B. W. Kernighan, D. Richie" ,"C Programming Language",
+                "Know as the bible of C, this classic bestseller introduces", 14.98m),
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -22,6 +25,11 @@ namespace Store.Memory
             return books.Where(book => book.Author.Contains(titlePart) 
                                     || book.Title.Contains(titlePart))
                 .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
